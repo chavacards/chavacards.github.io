7443480fde94ec94cfa8ddd3f6c4edd220ee6d65
Chava <salvador.klevergo@klevergo.com>
2023-06-29 14:26:30 -0500

    Buen comit, bug fixes for clock and project1

--- src/scripts/clock.js
+++ src/scripts/clock.js
@@ -5,9 +5,36 @@ function displayTime(){
     var sec = dateTime.getSeconds();
     var session= document.getElementById('session')
 
-    document.getElementById('hours').innerHTML = hrs;
-    document.getElementById('minutes').innerHTML = min;
-    document.getElementById('seconds').innerHTML = sec;
+    if(hrs >= 12){
+        session.innerHTML='pm';
+    } else{
+        session.innerHTML='am';
+    }
 
+    if(hrs > 12){
+        hrs = hrs -12;
+    }
+    if(hrs < 10){
+        document.getElementById('hours').innerHTML="0"+hrs;
+    }
+    else{
+        document.getElementById('hours').innerHTML = hrs;
+    }
+
+    if(min < 10){
+        document.getElementById('minutes').innerHTML="0"+min;
+    }
+    else{
+        document.getElementById('minutes').innerHTML = min;
+    }
+    
+    if(sec < 10){
+        document.getElementById('seconds').innerHTML="0"+sec;
+    }
+    else{
+        document.getElementById('seconds').innerHTML = sec;
+    }
+    
+    
 }
-setInterval(displayTime, 10);
+setInterval(displayTime, 1000);
--- src/scripts/project1.js
+++ src/scripts/project1.js
@@ -1,7 +1,9 @@
 const textToType= document.getElementById("textToType")
 const phrases=['Hello, nice to meet you!!! What is your[>~HV9', ' name?!?','Do you like dancing?', 'Would you dance with me?']
+const button=document.getElementById("fbutton")
 let i=0;
 let j=0;
+let cont=0;
 let array=[]
 
 function loop(){
@@ -17,7 +19,7 @@ function loop(){
         array.pop()
         i++
     }
-    else if(i<phrases[0].length + 12){
+    else if(i<phrases[0].length + 16){
         
         array.push(phrases[1][j])
         j++
@@ -26,22 +28,28 @@ function loop(){
     else{
         i++
         
-    }
-    
-    setTimeout(loop,300)
+    } 
+    setTimeout(loop,70)
 }
 
 ///loop()
 
-window.addEventListener('scroll', ()=>{
+function chispa(){
     const scrollable = document.documentElement.scrollHeight-window.innerHeight;
     const scrolled = window.scrollY;
     const punto_rico= scrolled/scrollable;
     //console.log(punto_rico);
 
-    if(punto_rico>=.58){
+    if(punto_rico>=.58 && cont<1){
         //console.log('js')
         loop()
+        cont++
     }
     
-})
+}
+
+
+window.addEventListener('scroll', chispa);
+
+
+button.addEventListener('click',console.log("holaaaa perrillooo"))
